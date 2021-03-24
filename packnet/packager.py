86977b4c148ce2e8212b43c7395e973708fad003
c0mplh4cks <jelmer943@gmail.com>
2021-03-24 16:51:35 +0100

    Internal code improvements, compatible with updated protocol classes

--- packnet/packager.py
+++ packnet/packager.py
@@ -12,9 +12,10 @@
 
 
 # === Importing Dependencies === #
-from .tree import Tree
+from . import Frame
+from . import Protocoltree
 from . import ETHERNET
-from . import RAW
+from . import ADDR
 
 
 
@@ -23,84 +24,119 @@ from . import RAW
 
 
 # === Packager === #
-class Packager():
-    def __init__(self, packet=b""):
-        self.packet = packet
+class Packager(Frame):
+    def __init__(self, startprotocol=ETHERNET.Header, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        self.layer = []
+        self.startprotocol = startprotocol
 
+        self.__src = ADDR()
+        self.__dst = ADDR()
 
+        for i in range(4):
+            setattr(self, f"l{i}", b"")
 
-    def checkprotocol(self, protocol):
-        if protocol.protocol == None: return RAW.Header
-
-        l = protocol.protocol
-        l = [l] if type(l) == int else l
-
-        for p in list(l):
-            child = Tree().get( parent=type(protocol), edge=protocol.protocol )
-            if child: return child
-
-        return RAW.Header
+        self.structure = [
+            f"l{i}" for i in range(4)
+        ]
 
 
-
-    def rcheckprotocol(self, protocol):
-        nodes = Tree().getnodes( child=type(protocol) )
-        
-        if nodes:
-            parent, child, edge = nodes[0]
-            result = parent()
-            result.protocol = edge
-
-            return result
+    def __getitem__(self, key):
+        if (-1 >= key > -5):
+            key = 4+key
+        if (0 <= key < 4):
+            return getattr(self, f"l{key}")
+        raise IndexError
 
 
+    def __setitem__(self, key, value):
+        if (-1 >= key > -5):
+            key = 4-key
+        if (0 <= key < 4):
+            return setattr(self, f"l{key}", value)
+        raise IndexError
 
-    def read(self, next=ETHERNET.Header):
-        packet = self.packet
-        self.layer = []
+
+
+    @property
+    def src(self):
+        if self[0]: self.__src.mac = self[0].src.mac
+        if self[1]: self.__src.ip = self[1].src.ip
+        if self[2]: self.__src.port = self[2].src.port
+        return self.__src
+
+
+    @src.setter
+    def src(self, value):
+        self.__src.addr = value
+        for protocol in self:
+            if type(protocol) != bytes:
+                if hasattr(protocol, "src"):
+                    protocol.src.addr = self.__src.addr
+
+
+    @property
+    def dst(self):
+        if self[0]: self.__dst.mac = self[0].dst.mac
+        if self[1]: self.__dst.ip = self[1].dst.ip
+        if self[2]: self.__dst.port = self[2].dst.port
+        return self.__dst
+
+
+    @dst.setter
+    def dst(self, value):
+        self.__dst.addr = value
+        for protocol in self:
+            if type(protocol) != bytes:
+                if hasattr(protocol, "dst"):
+                    protocol.dst.addr = self.__dst.addr
+
+
+
+    def fill(self, protocol):
+        protocols = [ protocol ]
 
         while True:
-            current = next
-            protocol = current(packet)
-            protocol.read()
+            node = Protocoltree.getnodes( child=protocol )
+            if not node: break
+            protocol = node[0][0]
+            protocols.insert( 0, protocol )
 
-            self.layer.append( protocol )
+        for i, protocol in enumerate(protocols):
+            self[i] = protocol()
 
-            if type(protocol) == RAW.Header: break
-
-            packet = protocol.data
-            next = self.checkprotocol( protocol )
-
-        return len(self.layer)
-
-
-
-    def fill(self, protocol, src=None, dst=None):
-        if type( protocol ) == RAW.Header: return None
-        self.layer = [RAW.Header()]
-
-        while True:
-            self.layer.insert( 0, protocol )
-            protocol = self.rcheckprotocol( protocol )
-            if not protocol: break
-
-        for p in self.layer:
-            if hasattr(p, "src") and src:
-                p.src = src.copy()
-            if hasattr(p, "dst") and dst:
-                p.dst = dst.copy()
-
-        return len(self.layer)
+        for i, protocol in enumerate(protocols[:-1]):
+            edge = Protocoltree.get( parent=type(self[i]), child=type(self[i+1]) )
+            if type(self[i].protocol) != list:
+                self[i].protocol.integer = edge
 
 
+    def to_bytes(self, *args, **kwargs):
+        packet = b""
 
-    def build(self):
-        for i in range( len(self.layer) )[::-1]:
-            self.layer[i].build()
-            self.layer[i-1].data += self.layer[i].packet
+        for i in range(1, 4)[::-1]:
+            if type( self[i] ) != bytes:
+                self[i-1].payload = self[i].packet
 
-        self.packet = self.layer[0].packet
+        return (self[0].packet + self.payload)
 
-        return len(self.packet)
+
+    def from_bytes(self, packet=b"", *args, **kwargs):
+        protocol = self.startprotocol
+        i = 0
+
+        for l in range(4):
+            self[l] = protocol()
+            i += self[l].from_bytes( packet[i:] )[0]
+            next = self[l].protocol
+            if next == None: break
+            next = next if type(next) == list else [int(next)]
+            for edge in next:
+                protocol = Protocoltree.get( parent=protocol, edge=edge )
+                if protocol: break
+            if protocol == None: break
+
+        self.src = self.src
+        self.dst = self.dst
+
+        return (i,)
